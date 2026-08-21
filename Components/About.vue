457bb1b63ae023bfac307405807e419ea2766279
spystar20 <hert5197@gmail.com>
2026-08-21 13:12:33 +0530

    .

--- Components/About.vue
+++ Components/About.vue
@@ -64,10 +64,11 @@ tl.from(".img-strips div",{
 
 </script>
 <template>
- <div class="md:p-5 p-2 flex  bg-white relative z-10  flex-col md:flex-row gap-8 container ">
+    
+ <div class="md:p-5 p-2 grid md:grid-cols-[7fr_3fr]  bg-white relative z-10   gap-6 container ">
             <!-- col-1 -->
-            <div class="flex-7 ">
-                <div  class=" rounded-2xl md:rounded-4xl   h-screen flex flex-col items-start overflow-hidden  justify-end relative ">
+            <div>
+                <div  class=" rounded-2xl md:rounded-4xl   min-h-[calc(100vh-40px)] flex flex-col items-start overflow-hidden  justify-end relative ">
 <div class=" absolute inset-0  img-grid w-full h-full img-strips">
 <div style="background-image: url(https://i.pinimg.com/1200x/5d/2f/b8/5d2fb87f8c6aacb1d930e01e401f8a05.jpg
 ); background-position: 0% 0%; background-size: 100% 600%;"  class="w-full h-1/6 bg-cover"></div>
@@ -85,24 +86,24 @@ tl.from(".img-strips div",{
 
                     <div
                         class="flex flex-col items-start justify-start rounded-4xl text-black font-poppins capitalize z-40">
-                        <span class="pr-6 pt-4 rounded-tr-lg  bg-white leftText">
+                        <span class="md:pr-6 pr-3 pt-4 rounded-tr-lg  bg-white leftText">
                             <h6 class="rounded-full border px-3 py-1 text-sm  w-max whitespace-nowrap">gorgeous interior</h6>
                         </span>
-                        <span class="pr-6 py-2 rounded-tr-lg bg-white leftText">
+                        <span class="md:pr-6 pr-3 py-1 md:py-2 rounded-tr-lg bg-white leftText">
                             <h6
-                                class="font-bold text-6xl bg-linear-to-r from-black to-main text-transparent bg-clip-text  ">
+                                class="font-bold text-4xl md:text-6xl bg-linear-to-r from-black to-main text-transparent bg-clip-text  ">
                                 Modern</h6>
                         </span>
-                        <span class="md:pr-6 pr-16 py-2 rounded-tr-lg bg-white leftText">
+                        <span class="md:pr-6 pr-3 py-1 md:py-2 rounded-tr-lg bg-white leftText">
                             <h6
-                                class="font-bold text-6xl  bg-linear-to-r from-black to-main text-transparent bg-clip-text ">
+                                class="font-bold text-4xl md:text-6xl  bg-linear-to-r from-black to-main text-transparent bg-clip-text ">
                                 Minimalist</h6>
                         </span>
                     </div>
                 </div>
             </div>
             <!-- col-2 -->
-            <div class="flex-3 grid grid-rows-2 gap-6">
+            <div class=" grid grid-rows-2 gap-6 min-h-[calc(100vh-40px)] ">
 
                 <div
                     class="flex flex-col items-start justify-end gap-3 font-body rounded-2xl md:rounded-4xl bg-linear-120 from-black to-main text-white p-5 text-container">
